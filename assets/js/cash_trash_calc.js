86f77531df5daa20cbf91cf78def06baf4b8c07d
sporkinthetoad <98916370+sporkinthetoad@users.noreply.github.com>
2025-06-14 20:53:12 +0100

    added london status report

--- assets/js/cash_trash_calc.js
+++ assets/js/cash_trash_calc.js
@@ -4,35 +4,34 @@ let change = 0
 let certificate = document.createElement("h2");
 let img = document.createElement("img");
 img.src = "../assets/images/cash-in-the-trash/bin.png";
-let cash = document.createElement("img");
 let footer = document.querySelector("footer");
 
 output.innerHTML = slider.value;
+
+if (slider.value === 0) {
+  slider.value = 1;
+}
 
 slider.oninput = function() {
   output.innerHTML = this.value;
 }
 
 function submit() {
-    console.log(change)
-    if (change > 0) {
-      document.body.removeChild(certificate, img, cash);
+    if (change === 1) {
+      document.body.removeChild(certificate);
+      document.body.removeChild(img)
+      change -= 1
     }
     let submit_value = slider.value;
 
     if (submit_value % 2 != 0) {
-      submit_value -= 1;
-      
+        submit_value -= 1;
     }
 
     submit_value /= 2
 
-    if (submit_value > 0) {
-      certificate.innerText = `Certified ${submit_value} Cash in the Trash`;
-      document.body.insertBefore(certificate, footer);
-      document.body.insertBefore(img, footer);
-      change += 1
-      
-    }
+    certificate.innerText = `Certified ${submit_value} Cash in the Trash`;
+    document.body.insertBefore(certificate, footer);
+    document.body.insertBefore(img, footer);
+    change += 1
   }
-
